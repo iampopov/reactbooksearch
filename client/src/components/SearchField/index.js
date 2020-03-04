e9d0afb8a7fa7d3be559d437db54d9232448e6f6
iampopov <ivantsar@gmail.com>
2020-03-03 19:47:08 -0700

    wired the google api search

--- client/src/components/SearchField/index.js
+++ client/src/components/SearchField/index.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "../../utils/API"
 
 function Search() {
+
+const [books, setBooks] = useState({
+  name: ""
+})
+
+const [result, setResult] = useState({
+  bookName: "",
+  author: "",
+  image: "",
+  synopsis: "" 
+})
+
+useEffect(() => {
+     
+}, [])
+
+const handleInputChange = (e) => {
+  const { name, value } = e.target
+  setBooks({ name: value})
+}
+
+const handleFormSubmit = (e) => {
+  e.preventDefault()
+  API.searchBook(books.name)
+  //console.log(books.name) //  {"": "holmes"}  
+    .then(res => {
+      console.log(res.data.items[0].id);
+      // console.log(res.data.items.length);
+      
+      for (let i=0; i<res.data.items.length; i++) {
+      
+        //console.log(setResult);
+
+
+        
+        // console.log(res.data.items[i].volumeInfo.title);
+        
+      }
+      
+      // const searchRes = res.data.items.filter(item => item.volumeInfo.title)
+      // console.log(searchRes);
+      
+    })
+    .catch(err => console.log(err))
+}
+
   return (
     <>
     <div className="form-group">
-      <input className="form-control" />
+      <input onChange={handleInputChange} className="form-control" />
     </div>
-    <button  style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
+    <button onClick={handleFormSubmit} style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
     Search Author
     </button>
     </>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -16,5 +16,10 @@ export default {
   // Saves a book to the database
   saveBook: function(bookData) {
     return axios.post("/api/books", bookData);
+  },
+  searchBook: function(query) {
+    return axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${query}`
+    )
   }
 };
